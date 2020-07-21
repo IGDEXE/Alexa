48598eba5241424c40e8b98eba9b7ccb246506b1
Ivo Dias <igd753@outlook.com.br>
2020-07-21 15:57:39 -0300

    Create

---
+++ Windows Scripts/DesligarTela.ps1
@@ -0,0 +1,29 @@
+Add-Type -TypeDefinition '
+using System;
+using System.Runtime.InteropServices;
+ 
+namespace Utilities {
+   public static class Display
+   {
+      [DllImport("user32.dll", CharSet = CharSet.Auto)]
+      private static extern IntPtr SendMessage(
+         IntPtr hWnd,
+         UInt32 Msg,
+         IntPtr wParam,
+         IntPtr lParam
+      );
+ 
+      public static void PowerOff ()
+      {
+         SendMessage(
+            (IntPtr)0xffff, // HWND_BROADCAST
+            0x0112,         // WM_SYSCOMMAND
+            (IntPtr)0xf170, // SC_MONITORPOWER
+            (IntPtr)0x0002  // POWER_OFF
+         );
+      }
+   }
+}
+'
+
+[Utilities.Display]::PowerOff()
